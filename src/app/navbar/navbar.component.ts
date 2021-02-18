e4f1ae0fab784e5ae1e9f7ed7526835bd11e11b0
Dishan Fernando <dishanf@irononetech.com>
2021-02-18 10:36:15 +0530

    updates

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { environment } from "../../environments/environment";
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
-  selector: "navbar",
-  templateUrl: "./navbar.component.html",
-  styleUrls: ["./navbar.component.css"],
+  selector: 'navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
   backgroundColor = environment.navBarBackgroundColor;
